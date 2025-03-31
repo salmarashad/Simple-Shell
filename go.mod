07a0a43626f5fb47f0fcce1846aec7b498572923
salmarashad <salmamarashad@gmail.com>
2025-03-31 18:45:38 +0200

    adding history (up and down arrows)

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module simple-shell
 
 go 1.24.1
+
+require (
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-tty v0.0.7 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/term v0.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,54 +1,108 @@
 package main
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/mattn/go-tty"
 )
 
-
 var history []string
+var historyIndex int
 
-func main (){
-	reader := bufio.NewReader(os.Stdin)
+func main() {
 
-	for{
+	// to detect up and down arrows
+	tty, err := tty.Open()
+	if err != nil {
+		log.Fatalf("Failed to open tty: %v", err)
+	}
+	defer tty.Close()
 
-		var rootDirectory, rootError = os.Getwd()
+	// intilise history
+	history = []string{}
+	historyIndex = 0
 
+	
+	var currentInput string
+
+	for {
+		// get and display current directory
+		rootDirectory, rootError := os.Getwd()
 		if rootError != nil {
 			log.Fatalf("Failed to get current directory: %v", rootError)
 		}
 
-		fmt.Print("> ", rootDirectory ," % ")
+		// clear the current line and reprint with current input
+		fmt.Print("\r\033[K") // clear the line
+		fmt.Print("> ", rootDirectory, " % ", currentInput)
 
-		// read the input 
-		input, err := reader.ReadString('\n')
-
-		// adding to my history of commands 
-		history = append(history, input)
-
+		// read a single key press
+		nextKey, err := tty.ReadRune()
 		if err != nil {
-			fmt.Fprint(os.Stderr, err)
+			log.Fatal(err)
 		}
 
-		// handle the execution of the input
-        if err = executeInput(input); err != nil {
-            fmt.Fprint(os.Stderr, err)
-        }
+		// handle special keys
+		switch nextKey {
+			case 27: // arrow key start
+			next, err := tty.ReadRune()
+			if err != nil {
+				continue
+			}
+		
+			if next != 91 { // if the next character isn't '[', ignore
+				continue
+			}
+		
+			key, err := tty.ReadRune()
+			if err != nil {
+				continue
+			}
+		
+			if key == 65 { // up arrow
+				if historyIndex > 0 {
+					historyIndex--
+					currentInput = history[historyIndex]
+				}
+			} else if key == 66 { // down arrow
+				if historyIndex < len(history)-1 {
+					historyIndex++
+					currentInput = history[historyIndex]
+				} else if historyIndex == len(history)-1 {
+					historyIndex++
+					currentInput = ""
+				}
+			}
+		case 13: // enter key
+			fmt.Println() // move to next line
+			if currentInput != "" {
+				// add to history
+				history = append(history, currentInput)
+				historyIndex = len(history)
 
-		fmt.Println(history)
-
+				// execjte the command
+				err := executeInput(currentInput)
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+				}
+				currentInput = ""
+			}
+		case 8, 127: // backspace handling
+			if len(currentInput) > 0 {
+				currentInput = currentInput[:len(currentInput)-1]
+			}
+		default:
+			currentInput += string(nextKey)
+		}
 	}
-
 }
 
-func executeInput (input string ) error{
-	
+func executeInput(input string) error {
 	// removing the new line character
 	input = strings.TrimSuffix(input,"\n")
 
